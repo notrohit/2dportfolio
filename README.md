fa3e3b5ba9224dfad8f1c4bfdcbe4092ad944fc9
Rohit <rohitsingh250193@gmail.com>
2023-09-24 11:14:27 +0530

    add 2d portfolio draft

---
+++ README.md
@@ -0,0 +1,1 @@
+Preview coming soon
---
+++ src/data/WorkData.js
@@ -0,0 +1,13 @@
+// work data
+
+export const Work = [
+  {
+    id: 1,
+    name: "Agency Landing Page",
+    description:
+      "It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
+    tags: ["react", "gsap", "styledComponents"],
+
+    demo: "https://agency-landing-pink.vercel.app/",
+  },
+];
